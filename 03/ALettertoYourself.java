df66d02e89460356d357a43577548aa94db80919
Jchoe1 <36394885+Jchoe1@users.noreply.github.com>
2018-02-16 10:50:15 -0500

    Add files via upload

---
+++ 03/ALettertoYourself.java
@@ -0,0 +1,17 @@
+public class ALettertoYourself{
+	public static void main (String[] args){
+ 		System.out.println("-----------------------------------------------");
+		System.out.println("|                                         ####|");
+		System.out.println("|                                         ####|");
+		System.out.println("|                                         ####|");
+		System.out.println("|                                             |");
+		System.out.println("|                                             |");
+		System.out.println("|                                             |");
+		System.out.println("|                    Jonathan Smith           |");
+		System.out.println("|                    193 Water Way            |");
+		System.out.println("|                    Rochman, VI 90123        |");
+		System.out.println("|                                             |");
+ 		System.out.println("-----------------------------------------------");
+	}
+}
+
